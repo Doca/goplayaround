7ba7c5fd2abe4cc31a749d2456125b8049b0e88d
Ralph Caraveo <deckarep@gmail.com>
2020-04-19 11:43:18 -0700

    fixed worker pool design

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ import (
 // exit status 66
 // go run --race main.go
 
-const(
+const (
 	maxPoolSize = 1000
 )
 
@@ -35,28 +35,37 @@ func (c *OccurenceCounter) Value(key uint32) int {
 	defer c.mux.RUnlock()
 	return c.v[key]
 }
+
 func (c *OccurenceCounter) Save() {
 	c.mux.Lock()
 	defer c.mux.Unlock()
 
-
 	// 1. In Go, Locks are not re-entrant...
 
-
-	// be able to return a current state of the mutex
-	// in copying the values into a new map
-
-	total := 0
-	for _, v := range c.v {
-		total = total + v
-	}
+	total := c.agg()
 
 	// Simulates the save into PG
-	fmt.Println("total:",total)
+	fmt.Println("Save into Postgres total:", total)
 
 	// cleaning up the mutex
 	c.clear()
 }
+
+func (c *OccurenceCounter) Total() int {
+	c.mux.RLock()
+	defer c.mux.RUnlock()
+
+	return c.agg()
+}
+
+func (c *OccurenceCounter) agg() int {
+	total := 0
+	for _, v := range c.v {
+		total = total + v
+	}
+	return total
+}
+
 func (c *OccurenceCounter) Clear() {
 	c.mux.Lock()
 	c.clear()
@@ -67,117 +76,80 @@ func (c *OccurenceCounter) clear() {
 	c.v = make(map[uint32]int)
 }
 
-
 func CountOccurance(i uint32, m *OccurenceCounter) {
 	m.Inc(i)
 }
 
 func main() {
+
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
 	ticker := time.NewTicker(2 * time.Second)
 	quitTicker := make(chan struct{})
-	//done := make(chan bool, 1)
-	oc := OccurenceCounter{v: make(map[uint32]int)}
-	stopMainLoop := false
+	flushDone := make(chan int)
 
+	oc := &OccurenceCounter{v: make(map[uint32]int)}
 
-	wg := &sync.WaitGroup{}
-
-	// have an interval to write to postgres
-	wg.Add(1)
+	// Periodically write to Postgres.
 	go func(mx *OccurenceCounter) {
-		defer wg.Done()
-		//defer func() {
-		//	fmt.Println(" initial goroutine is complete")
-		//	wg.Done()
-		//}()
-
 		for {
 			select {
 			case <-ticker.C:
 				// main logic
 				mx.Save()
 			case <-quitTicker:
-				stopMainLoop = true
-				fmt.Println("quitting...")
-				fmt.Println("If there is still data in OccurenceCounter write it away")
-				//val := mx.Values()
-				//fmt.Println(val)
 				ticker.Stop()
 
+				// Attempt one final save.
+				mx.Save()
 
+				// Signal final flush is completed.
+				close(flushDone)
 				return
 			}
 
 		}
-	}(&oc)
+	}(oc)
 
-	// main loop
-	//wp := workerpool.New(maxPoolSize)
+	// Bound worker-pool with max goroutines.
 	sem := make(chan int, maxPoolSize)
-	//wp.Submit(func() {
-	//	fmt.Println("Test123")
-	//})
 
-	fmt.Println("Starting process...")
-
-
-
-	go func(){
+	go func() {
+		var myWG sync.WaitGroup
 		for {
-			wg.Add(1)
-			sem <-1
-			go func(){
-				defer wg.Done()
-				theInt := uint32(rand.Intn(100))
-				CountOccurance(theInt, &oc)
-				<-sem
-			}()
+			select {
+			// Upon sigterm, start teardown.
+			case <-sigs:
+				// Wait for all worker items to finish.
+				myWG.Wait()
+
+				// Ask for tear down ticker.
+				close(quitTicker)
+				return
+			default:
+				// Otherwise, continue to enqueue.
+				myWG.Add(1)
+				sem <- 1
+				go func() {
+					defer myWG.Done()
+					enqueueTask(&myWG, oc)
+					<-sem
+				}()
+			}
 		}
 	}()
 
-	<- sigs
-	fmt.Println("wait for waitgroup")
-	close(quitTicker)
+	// Wait for final flush from ticker tear down.
+	<-flushDone
 
-	wg.Wait()
-
-
-
-	// One final flush of anything in oc.
-	oc.Save()
+	// Ensure zero result.
+	fmt.Println("Final total: ", oc.Total())
 
 	fmt.Println("Shutdown success...")
+}
 
-	//for {
-	//
-	//	if stopMainLoop == true {
-	//		break
-	//	}
-	//
-	//	theInt := uint32(rand.Intn(100))
-	//	// Fine
-	//
-	//	wp.Submit(func(){CountOccurance(theInt, &oc)})
-	//	//wp.Submit(func(){
-	//	//	sig := <-sigs
-	//	//	fmt.Println()
-	//	//	fmt.Println(sig)
-	//	//	done <- true
-	//	//})
-	//
-	//	// where to exit the process so that no data is being lost?
-	//
-	//	//wp.Submit(func(){
-	//	//	<-done
-	//	//	fmt.Println("exiting")
-	//	//	time.Sleep(5 * time.Second)
-	//	//	close(quitTicker)
-	//	//
-	//	//})
-	//
-	//
-	//
-	//}
+func enqueueTask(wg *sync.WaitGroup, oc *OccurenceCounter) {
+	theInt := uint32(rand.Intn(100))
+	CountOccurance(theInt, oc)
 }
